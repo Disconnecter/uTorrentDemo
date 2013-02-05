ef0fda5922c76dcae9075c7adb90566dd702a22c
Disconnecter <zabolotnyy.s@gmail.com>
2013-02-05 09:23:44 +0200

    Initial Commit

---
+++ uTorrentDemo/UTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  UTAppDelegate.h
+//  uTorrentDemo
+//
+//  Created by Zabolotnyy Sergey on 2/5/13.
+//  Copyright (c) 2013 Zabolotnyy Sergey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ uTorrentDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  uTorrentDemo
+//
+//  Created by Zabolotnyy Sergey on 2/5/13.
+//  Copyright (c) 2013 Zabolotnyy Sergey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "UTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([UTAppDelegate class]));
+    }
+}
